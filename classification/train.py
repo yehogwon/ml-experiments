c0dd7d1cee591ee86387d2b5f588f7a6442f714b
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-08-15 21:47:57 +0900

    overall training

--- classification/train.py
+++ classification/train.py
@@ -5,11 +5,54 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
 
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.datasets import VisionDataset
+
+import wandb
 
 from common import create_model
 
+class Trainer: 
+    def __init__(self, exp_name: str, dataset: VisionDataset, model: nn.Module, ckpt_path: str, device: str='cpu') -> None:
+        self.exp_name = exp_name
+        self.dataset = dataset
+        self.model = model
+        self.ckpt_path = ckpt_path
+        self.device = device
+
+        wandb.init(project='Classification Experiment', name=self.exp_name)
+
+    def train(self, batch_size: int, n_epoch: int, lr: float, weight_decay: float) -> None: 
+        # TODO: implement training pipeline for image classification
+        pass
+
+class ActiveLearningTrainer(Trainer):
+    def __init__(self, exp_name: str, dataset: VisionDataset, model: torch.nn.Module, ckpt_path: str, n_stages: int, budget_per_stage: int, cost_function: str, device: str='cpu') -> None:
+        super().__init__(self, exp_name, dataset, model, ckpt_path, device)
+        self.n_stages = n_stages
+        self.budget_per_stage = budget_per_stage
+
+        # TODO: cost function
+
+    def train(self) -> None: 
+        # TODO: implement training pipeline for active learning for image classification
+        pass
+
 def main(args: argparse.Namespace): 
-    pass
+    model = create_model(args.model)
+
+    if args.pretrained_model:
+        print(f'Load pretrained model: {args.pretrained_model}')
+        model.load_state_dict(torch.load(args.pretrained_model))
+    
+    if args.al:
+        al = ActiveLearningTrainer(args.exp_name, args.dataset, model, args.ckpt_path, args.al_stage, args.budget_per_stage, args.cost_function)
+        al.train(args.batch_size, args.epochs, args.lr, args.weight_decay)
+    else:
+        trainer = Trainer(args.exp_name, args.dataset, model, args.ckpt_path)
+        trainer.train(args.batch_size, args.epochs, args.lr, args.weight_decay)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -27,6 +70,8 @@ if __name__ == '__main__':
     parser.add_argument('--al_stage', type=int)
     parser.add_argument('--budget_per_stage', type=int)
     parser.add_argument('--cost_function', type=str)
+
+    parser.add_argument('--exp_name', type=str)
     
     args = parser.parse_args()
     main(args)
