ecb9c08a212aadd6a2a247d3cc298eb7f2bb207a
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-08-18 21:19:07 +0900

    separate training iteration as a func

--- classification/train.py
+++ classification/train.py
@@ -58,44 +58,46 @@ class Trainer:
         self.model.train()
 
         for epoch in range(start_epoch, n_epoch + 1):
-            losses = []
-            n_correct = 0
-            for x, y in tqdm(train_loader, desc=f'Epoch {epoch}/{n_epoch}'):
-                x, y = x.to(self.device), y.to(self.device)
-                optimizer.zero_grad()
-                y_pred = self.model(x)
-                n_correct += (F.softmax(y_pred, dim=1).argmax(dim=1) == y).sum().item()
-                loss = loss_fn(y_pred, y)
-                loss.backward()
-                optimizer.step()
+            train_loss, train_acc = self._train_iteration(train_loader, loss_fn, optimizer, desc=f'Epoch {epoch}/{n_epoch}', wandb_log=True)
 
-                losses.append(loss.item())
-                wandb.log({'loss': loss.item()})
-
-            train_loss = sum(losses) / len(losses)
-
-            if self.dataset_name == 'imagenet': 
-                acc = n_correct / len(self.train_dataset)
-                print(f'Epoch {epoch} | Train Loss: {train_loss} | Accuracy: {acc}')
-                wandb.log({
-                    'epoch': epoch, 
-                    'train_loss': train_loss, 
-                    'accuracy': acc
+            log_info = {
+                'epoch': epoch, 
+                'train_loss': train_loss, 
+                'train_acc': train_acc
+            }
+            # TODO: ImageNet train split would be available soon
+            if self.dataset_name != 'imagenet': 
+                val_acc, val_loss = self.validate(batch_size) # run validation only on CIFAR-10/100
+                log_info.update({
+                    'val_loss': val_loss, 
+                    'val_acc': val_acc
                 })
-            else: 
-                acc, test_loss = self.validate(batch_size)
-                print(f'Epoch {epoch} | Train Loss: {train_loss} | Test Loss: {test_loss} | Accuracy: {acc}')
-                wandb.log({
-                    'epoch': epoch, 
-                    'train_loss': train_loss, 
-                    'test_loss': test_loss,
-                    'accuracy': acc
-                })
+
+            print(' | '.join([f'{k.title()}: {v}' for k, v in log_info.items()]))
+            wandb.log(log_info)
 
             if epoch % self.ckpt_interval == 0:
-                ckpt_saved = os.path.join(self.ckpt_path, f'{self.exp_name}_{epoch}.pth')
-                torch.save(self.model.state_dict(), ckpt_saved)
-                print(f'Checkpoint saved: {ckpt_saved}')
+                ckpt_path = self._save_model(f'{self.exp_name}_{epoch}.pth')
+                print(f'Checkpoint saved: {ckpt_path}')
+    
+    def _train_iteration(self, dataloader: DataLoader, loss_fn: nn.Module, optimizer: optim.Optimizer, desc: str='Training', wandb_log: bool=True) -> tuple[float, float]:
+        losses = []
+        n_correct = 0
+        
+        for x, y in tqdm(dataloader, desc=desc):
+            x, y = x.to(self.device), y.to(self.device)
+            optimizer.zero_grad()
+            y_pred = self.model(x)
+            n_correct += (F.softmax(y_pred, dim=1).argmax(dim=1) == y).sum().item()
+            loss = loss_fn(y_pred, y)
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss.item())
+            if wandb_log: 
+                wandb.log({'loss': loss.item()})
+        
+        return sum(losses) / len(losses), n_correct / len(self.train_dataset) # loss_avg, acc
     
     def validate(self, batch_size: int) -> float: 
         test_loader = DataLoader(self.test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
@@ -113,6 +115,11 @@ class Trainer:
         acc = n_correct / len(self.test_dataset)
         loss_avg = sum(losses) / len(losses)
         return acc, loss_avg
+
+    def _save_model(self, name: str) -> str: 
+        ckpt_saved = os.path.join(self.ckpt_path, name)
+        torch.save(self.model.state_dict(), ckpt_saved)
+        return ckpt_saved # return the path to saved checkpoint
 
 class ActiveLearningTrainer(Trainer):
     def __init__(self, exp_name: str, dataset: str, transform, model: nn.Module, ckpt_path: str, ckpt_interval: int, acquisition_function: str, device: str='cpu') -> None:
@@ -146,16 +153,25 @@ class ActiveLearningTrainer(Trainer):
             'ckpt_interval': self.ckpt_interval
         })
 
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = optim.AdamW(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+
+        self.model.to(self.device)
+        self.model.train()
+
         for stage in range(1, n_stages + 1): 
             # sampling: get indices to train on
             # label_indices: np.ndarray <- indices to newly label
             if stage == 1: 
+                assert self._remaining_samples() >= budget_per_stage, f'Not enough samples to label: {self._remaining_samples()} < {budget_per_stage}'
                 label_indices = np.random.choice(np.where(self.labeled_ones == 0)[0], size=budget_per_stage, replace=False)
             else: 
                 if self._remaining_samples() < budget_per_stage: 
                     label_indices = np.where(self.labeled_ones == 0)[0]
                 else: 
                     label_indices = self._update_acquisition()[:budget_per_stage]
+            # For the reference of SubsetRandomSampler, refer to here: https://pytorch.org/docs/stable/data.html#torch.utils.data.SubsetRandomSampler
+            train_dataloader = DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, sampler=torch.utils.data.SubsetRandomSampler(label_indices))
     
     def _update_acquisition(self) -> list[int]: 
         acquisition_value_arr = np.array(self.acquisition_function(self.train_dataset, self.model, n_classes(self.dataset_name)), dtype=float)
