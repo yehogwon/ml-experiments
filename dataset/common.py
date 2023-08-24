753c2449ed997c18f1480c9c02e57d8061b03e9b
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-08-24 22:59:45 +0900

    voc support

--- dataset/common.py
+++ dataset/common.py
@@ -2,10 +2,7 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from torchvision.datasets import VisionDataset, CIFAR10, CIFAR100, ImageNet
-
-Cifar10 = CIFAR10
-Cifar100 = CIFAR100
+from torchvision.datasets import VisionDataset, CIFAR10, CIFAR100, ImageNet, VOCSegmentation
 
 def n_classes(name: str): 
     if name == 'cifar10': 
@@ -19,10 +16,11 @@ def n_classes(name: str):
 def create_dataset(name: str, train: bool, transform=None): 
     root = f'dataset/{name}'
     if name == 'cifar10': 
-        return Cifar10(root=root, train=train, transform=transform, download=False)
+        return CIFAR10(root=root, train=train, transform=transform, download=False)
     elif name == 'cifar100': 
-        return Cifar100(root=root, train=train, transform=transform, download=False)
+        return CIFAR100(root=root, train=train, transform=transform, download=False)
     elif name == 'imagenet':
         return ImageNet(root=root, split='train' if train else 'val', transform=transform)
-        # return ImageNet(root=root, split='val', transform=transform) # train set is too large
+    elif name == 'voc': 
+        return VOCSegmentation(root=root, year='2012', image_set='train' if train else 'val', transform=transform)
     raise ValueError(f'Unknown dataset: {name}')
